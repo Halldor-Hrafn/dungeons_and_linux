8c3989fad7876a05e0935ffb1d43ec199e55e23f
Halldór Hrafn Reynisson <Halldor.hrafn.reynisson@gmail.com>
2023-04-01 19:27:34 +0000

    Added some more values to the playable character struct

--- src/character/playable_character.rs
+++ src/character/playable_character.rs
@@ -5,21 +5,34 @@ pub struct PlayableCharacter {
     first_name: String,
     surname: String,
     age: u16,
-    traits: String,
+    heigth: u16,
+    weight: u16,
+    eyes: String,
+    skin: String,
+    hair: String,
+    traits: Vec<String>,
+    features: Vec<String>,
     max_health: u16,
     level: u8,
     class: String,
-    stats: Vec<HashMap>,
+    sub_class: String,
+    stats: HashMap,
     proficiency_bonus: u8,
     proficiencies: Vec<String>,
     other_proficiencies: Vec<String>,
     languages: Vec<String>,
     equipment: Vec<String>,
-    coins: Vec<HashMap>,
+    coins: HashMap,
     personality: String,
     ideals: String,
     bonds: String,
     flaws: String,
+    faction_affiliation: Vec<String>,
+    backstory: String,
+    spellcasting_ability: String,
+    spell_save_dc: u8,
+    spell_attack_bonus: u8,
+    spells: HashMap
 }
 
 impl PlayableCharacter {
@@ -28,6 +41,8 @@ impl PlayableCharacter {
         // todo: find a way to generate a class with realistic stats
         // todo: find a way to generate random equipments and proficiencies
         // todo: find a way to generate random ideals, bonds, flaws, etc.
+        // todo: find a way to generate random spells
+        // todo: find a way to do all of this shit
 
         // maybe add a lib.rs and some traits once I've worked more on this
     }
